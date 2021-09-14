a48c9cb34a1989d212cc754a1a61011e5a3ffb6b
now-processing <23730688+now-processing@users.noreply.github.com>
2021-09-13 20:59:55 -0500

    Poor readability back to multiple println

--- src/Assignment1_1.java
+++ src/Assignment1_1.java
@@ -1,5 +1,8 @@
 public class Assignment1_1 {
     public static void main(String[] args) throws Exception {
-        System.out.println("Michael Hill \nJava Web \nOmaha, NE \nBanana Cream Pie");
+        System.out.println("Michael Hill");
+        System.out.println("Java Web");
+        System.out.println("Omaha, NE");
+        System.out.println("Banana Cream Pie");
     }
 }
