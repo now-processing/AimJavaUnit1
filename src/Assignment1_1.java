b65d2fea3bf3bf5c583871ed6482b34fdad82742
now-processing <23730688+now-processing@users.noreply.github.com>
2021-09-13 20:54:09 -0500

    Assignments 1 and 2

---
+++ src/Assignment1_1.java
@@ -0,0 +1,8 @@
+public class Assignment1_1 {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Michael Hill");
+        System.out.println("Java Web");
+        System.out.println("Omaha, NE");
+        System.out.println("Banana Cream Pie");
+    }
+}
